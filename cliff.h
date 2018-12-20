8129eab97d0e5ab9d0b3021c8b27a7dbd9dc2e99
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-12-20 14:21:56 +0100

    Reduced header file surface.

--- cliff.h
+++ cliff.h
@@ -4,7 +4,7 @@
 // Distributed under the terms of the 2-clause BSD License. The full
 // license is in the file LICENSE, distributed as part of this software.
 
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdint.h>
 
 
