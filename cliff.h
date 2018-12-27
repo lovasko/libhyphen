d7995156196353113de32e9b3af37833af5cba72
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-12-27 14:48:31 +0100

    Rebranding code from 'cliff' to 'hyphen'.

--- cliff.h
+++ cliff.h
@@ -9,11 +9,11 @@
 
 
 /// Command-line interface.
-typedef struct cliff cliff;
+typedef struct hyphen hyphen;
 
 /// Create and (possibly) attach a new command.
-uint8_t cliff_cmd(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_cmd(
+  hyphen* cli,         ///< Command-line interface.
   uint64_t* cid,       ///< New returned command ID.
   const uint64_t pcid, ///< Parent command ID to attach to (0 for no parent).
   const char* name,    ///< Name.
@@ -23,8 +23,8 @@ uint8_t cliff_cmd(
 );
 
 /// Create and attach a new option.
-uint8_t cliff_opt(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_opt(
+  hyphen* cli,         ///< Command-line interface.
   const uint64_t cid,  ///< Command ID to attach to.
   const char shtn,     ///< Short name (\0 for no short name).
   const char* lngn,    ///< Long name (NULL for no long name).
@@ -35,8 +35,8 @@ uint8_t cliff_opt(
 );
 
 /// Create and attach a new flag.
-uint8_t cliff_flg(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_flg(
+  hyphen* cli,         ///< Command-line interface.
   const uint64_t cid,  ///< Command ID to attach to.
   const char shtn,     ///< Short name (\0 for no short name).
   const char* lngn,    ///< Long name (NULL for no long name).
@@ -46,8 +46,8 @@ uint8_t cliff_flg(
 );
 
 /// Create and attach a new argument.
-uint8_t cliff_arg(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_arg(
+  hyphen* cli,         ///< Command-line interface.
   const uint64_t cid,  ///< Command ID to attach to.
   const char* name,    ///< Name.
   const uint64_t cnt,  ///< Number of repeats (UINT64_MAX for all remaining).
@@ -55,16 +55,16 @@ uint8_t cliff_arg(
 );
 
 /// Skip a number of bytes in the storage structure.
-uint8_t cliff_pad(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_pad(
+  hyphen* cli,         ///< Command-line interface.
   const uint64_t cid,  ///< Command ID.
   const size_t size    ///< Number of bytes to skip.
 );
 
 /// Parse the command-line arguments. This function frees all memory allocated
 /// the build functions.
-uint8_t cliff_run(
-  cliff* cli,          ///< Command-line interface.
+uint8_t hyphen_run(
+  hyphen* cli,         ///< Command-line interface.
   const uint64_t rcid, ///< Root command ID (name used as program name).
   const int argc,      ///< Argument count.
   const char* argv[],  ///< Argument vector.
@@ -72,8 +72,8 @@ uint8_t cliff_run(
 );
 
 /// Convert an error code to an English description.
-void cliff_err(
-  cliff* cli,         ///< Command-line interface.
+void hyphen_err(
+  hyphen* cli,        ///< Command-line interface.
   const uint8_t code, ///< Function return/error code.
   const char** estr   ///< English description.
 );
